9f412005b8f39cb53e6b8a7d2625d620ab950507
aschneiderGit <aimeschneider68@gmail.com>
2025-10-29 12:54:56 +0100

    v-model

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="MetaBrain.org" />
   <PAGES />
+  <PageOne />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import PAGES from "./components/New-onef.vue";
+import PageOne from "./components/pages.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
     PAGES,
+    PageOne,
   },
 };
 </script>
---
+++ src/components/pages.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Two Way Binding</h1>
+  <input type="text" placeholder="Add the number" v-model="count" />
+  <h3>{{ count }}</h3>
+</template>
+<script>
+export default {
+  name: "PageOne",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
