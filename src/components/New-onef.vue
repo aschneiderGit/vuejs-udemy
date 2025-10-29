493b217dc5f83eab7517b4b0011c8c49bb2bb643
aschneiderGit <aimeschneider68@gmail.com>
2025-10-29 12:23:34 +0100

    components properties and interpolation

--- src/components/New-onef.vue
+++ src/components/New-onef.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>Hello</h1>
+  <h1>Email : {{ email }}</h1>
+  <h1>Mobile : {{ (mobile = "0333333") }}</h1>
+  <h1>Name : {{ getName1("Sajad") }}</h1>
 </template>
 <script>
 export default {
   name: "PAGES",
+  data() {
+    return {
+      email: "abc@gmail.com",
+      mobile: "555555",
+      getName1: function (a) {
+        return a;
+      },
+    };
+  },
 };
 </script>
 <style scoped>
