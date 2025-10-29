4407521c96df485e38c6c3dbd9418382cc16c507
aschneiderGit <aimeschneider68@gmail.com>
2025-10-29 12:15:03 +0100

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="MetaBrain.org" />
+  <PAGES />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import PAGES from "./components/New-onef.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    PAGES,
   },
 };
 </script>
---
+++ src/components/New-onef.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello</h1>
+</template>
+<script>
+export default {
+  name: "PAGES",
+};
+</script>
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
